323d83a526fea215ec5180475ed7d15e4a1cbac7
Arkhopravo <arkho.psarkar@gmail.com>
2024-06-17 18:06:14 +0530

    creating the frontend of the website using nextjs this is just creating a new nextjs file

--- lms-server/middleware/catchAsyncError.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-
-export const catchAsyncError = (theFunc: (req: Request, res: Response, next: NextFunction) => Promise<any>) => {
-    return (req: Request, res: Response, next: NextFunction) => {
-        Promise.resolve(theFunc(req, res, next)).catch(next);
-    };
-};
---
+++ lms-server/server.js
@@ -0,0 +1,43 @@
+// // import express, { json } from 'express'
+// const express = require('express')
+// const  { connect } =require( 'mongoose')
+// // Require the cloudinary library
+// const cloudinary = require('cloudinary').v2;
+// require('dotenv').config
+// const cors = require('cors')
+// const PORT = 4000
+// const app = express();
+
+
+
+
+
+// // connecting with mongodb
+// // connect(
+// //     process.env.MONGODB_URL
+// // )
+// // .then(() => {
+// //     console.log("Server is connected with the mongodb Database")
+// // }).catch(err => {
+// //     console.log("Database connection error: " + err)
+// // })
+
+
+// // use Cors 
+// app.use(cors({
+
+//     origin: ["http://localhost:4000","http://api_host.com"],
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true,
+// }))
+
+
+// app.get('/test', (req, res) => {
+//     res.send("Api is working")
+// })
+// // Listenting the port
+// app.listen(PORT, () => {
+//     console.log("Server listening on port " + PORT);
+// });
+
+
